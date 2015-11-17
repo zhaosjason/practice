1a3b0b6a874c3eb7558703d34d7e53d367d431a5
Jason Zhao <zhao473@purdue.edu>
2015-11-16 23:25:30 -0500

    Algorithm to find a run of integers with the largest sum from a list

---
+++ largest_run.java
@@ -0,0 +1,73 @@
+import java.util.*;
+
+public class largest_run{
+	public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter space deliniated run: ");
+		String[] temp = in.nextLine().split(" ");
+
+		int[] arr = new int[temp.length];
+		for(int i = 0; i < arr.length; i++){
+			arr[i] = Integer.parseInt(temp[i]);
+		}
+
+		System.out.println("sum=" + sum(arr, 0, arr.length));
+
+		int[] soln = getLargestRun(arr, 0, arr.length);
+		System.out.println("start=" + soln[0] + "   end=" + soln[1]);
+
+		for(int i = soln[0]; i < soln[1]; i++){
+			System.out.print(arr[i] + " ");
+		}
+
+		System.out.println();
+		System.out.println("sum=" + sum(arr, soln[0], soln[1]));
+	}
+
+	private static int[] getLargestRun(int[] arr, int begin, int end){
+		int sum = sum(arr, begin, end);
+		int pivot = findMinIndex(arr, begin, end);
+
+		// TODO: Don't return immediately, keep recursion going til -1 is hit.
+		//       Then compare values from getLargestRun, return largest value.
+		//       Bubble up until largest value reachest top of stack.  Return.
+		if(sum(arr, begin, pivot) > sum){
+			return getLargestRun(arr, begin, pivot);
+		}
+		else if(sum(arr, pivot + 1, end) > sum){
+			return getLargestRun(arr, pivot, end);
+		}
+		else{
+			int[] soln = new int[2];
+			soln[0] = begin;
+			soln[1] = end;
+
+			return soln;
+		}
+	}
+
+	private static int sum(int[] arr, int begin, int end){
+		int sum = 0;
+		for(int i = begin; i < end; i++){
+			sum += arr[i];
+		}
+
+		return sum;
+	}
+
+	private static int findMinIndex(int[] arr, int begin, int end){
+		int min = arr[begin];
+		int index = begin;
+		for(int i = begin + 1; i < end; i++){
+			if(arr[i] < min){
+				min = arr[i];
+				index = i;
+			}
+		}
+
+		if(min > 0)
+			return -1;
+
+		return index;
+	}
+}
