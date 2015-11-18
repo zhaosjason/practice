09802228ea762d84d246aa407aac7e514b2bf107
Jason Zhao <zhao473@purdue.edu>
2015-11-18 18:52:06 -0500

    Finished printTable and most of addEntry

---
+++ bible_vs_quran/hashtable.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <string.h>
+#include "hashtable.h"
+
+unsigned int hash(struct HashTable *ht, char *key){
+	unsigned long int index = 0;
+	int len = strlen(key);
+
+	int i;
+	for(i = 0; i < len; i++){
+		index += ((unsigned long int) key[i]) << (sizeof(int) * 8);
+		index %= ht->size;
+	}
+
+	return index;
+}
+
+int addEntry(struct HashTable *ht, char *key, int value){
+	printf("adding entry...\n");
+	//if(getEntry(ht, key))
+	//	return 0;
+
+	struct Node *node = malloc(sizeof(struct Node));
+	node->key = strdup(key);
+	node->value = value;
+	node->next = 0;
+
+	unsigned int index = hash(ht, key);
+	struct Node *temp = ht->arr[index];
+
+	if(!temp){
+		ht->arr[index] = node;
+	}
+	else{
+		// TODO: Check to see if entry with <key> already exists
+		while(temp->next)
+			temp = temp->next;
+
+		temp->next = node;
+	}
+
+	return 1;
+}
+
+int updateEntry(struct HashTable *ht, char *key, int value){
+	return -1;
+}
+
+int incrementEntry(struct HashTable *ht, char *key){
+	return -1;
+}
+
+int getEntry(struct HashTable *ht, char *key){
+	return -1;
+}
+
+int removeEntry(struct HashTable *ht, char *key){
+	return -1;
+}
+
+void printTable(struct HashTable *ht){
+	int i;
+	for(i = 0; i < ht->size; i++){
+		if(ht->arr[i]){
+			printf("[%d]:", i);
+
+			struct Node *n = ht->arr[i];
+			while(n){
+				printf(" %d", n->value);
+				n = n->next;
+			}
+
+			printf("\n");
+		}
+	}
+}
+
+void freeTable(struct HashTable *ht){
+	return;
+}
+
+
+
+
+
+
+
+
