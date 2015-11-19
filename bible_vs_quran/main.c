8d019877777c83d0e62eb971931554ae2787a096
Jason Zhao <zhao473@purdue.edu>
2015-11-19 00:51:52 -0500

    Added text parser and cleaner

--- bible_vs_quran/main.c
+++ bible_vs_quran/main.c
@@ -1,9 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 #include "hashtable.h"
 
+void testHashTable();
+
 int main(){
-	//FILE *fd = fopen("bible.txt", "r");
+	struct HashTable *bible;
+	unsigned int buckets = 5000000;
+
+	bible = malloc(sizeof(struct HashTable));
+	init(bible, buckets);
+
+	FILE *fd = fopen("bible.txt", "r");
+
+	char c;
+	int i = 0;
+	char buff[25];
+	while((c = fgetc(fd)) != EOF){
+		if(isalpha(c))
+			buff[i++] = c;
+		else if(c == ' '){
+			if(i > 0){
+				buff[i] = '\0';
+				if(!incrementEntry(bible, buff))
+					addEntry(bible, buff, 1);
+			}
+
+			memset(buff, 0, sizeof(buff));
+			i = 0;
+		}
+	}
+
+	fclose(fd);
+
+	printf("--------------- HashTable ---------------\n");
+	printTable(bible);
+	printf("-----------------------------------------\n");
+
+	//testHashTable();
+	freeTable(bible);
+}
+
+void testHashTable(){
 	struct HashTable *ht;
 	unsigned int buckets = 5000;
 	char word[100];
@@ -11,11 +51,32 @@ int main(){
 	ht = malloc(sizeof(struct HashTable));
 	init(ht, buckets);
 
+	int i = 0;
 	while(1){
+		// Adding
+		printf("addEntry: ");
 		scanf("%s", word);
-		addEntry(ht, word, 0);
+		addEntry(ht, word, i++);
+
+		getchar();
+
+		printf("increment? ");
+		char c = fgetc(stdin);
+		if(c == 'y')
+			incrementEntry(ht, word);
+
+		// Printing
 		printf("--------------- HashTable ---------------\n");
 		printTable(ht);
 		printf("-----------------------------------------\n");
+		
+		// Lookup
+		int temp;
+		printf("lookup: ");
+		scanf("%s", word);
+		if(getEntry(ht, word, &temp))
+			printf("%s->%d\n", word, temp);
+		else
+			printf("no entries found\n");
 	}
 }
